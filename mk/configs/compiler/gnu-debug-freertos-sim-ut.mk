f6b3e03f89e9aca1614243c9896d4a72aa0cc726
Jiaqi Liu <bucks99@Jiaqis-Air.wv.cc.cmu.edu>
2018-10-13 16:48:53 -0400

    working for mac

--- mk/configs/compiler/gnu-debug-freertos-sim-ut.mk
+++ mk/configs/compiler/gnu-debug-freertos-sim-ut.mk
@@ -10,42 +10,42 @@ include $(BUILD_ROOT)/mk/configs/compiler/freertos_sim_common.mk
 include $(BUILD_ROOT)/mk/configs/compiler/ut_flags.mk
 
 ifeq ("$(HOST)","Darwin")
-    CFLAGS :=   $(DEBUG) \
-                $(DARWIN_LLVM_CFLAGS) \
-                $(DARWIN_LLVM_INCLUDES) \
-                $(FREERTOS_SIM_FLAGS_COMMON) \
-                $(FREERTOS_SIM_INCLUDES_COMMON) \
-                $(COVERAGE) \
-                $(UT_FLAGS)
+CFLAGS :=   $(DEBUG) \
+$(DARWIN_LLVM_CFLAGS) \
+$(DARWIN_LLVM_INCLUDES) \
+$(FREERTOS_SIM_FLAGS_COMMON) \
+$(FREERTOS_SIM_INCLUDES_COMMON) \
+$(COVERAGE) \
+$(UT_FLAGS)
 
-    CXXFLAGS := $(DEBUG) \
-                $(DARWIN_LLVM_CXXFLAGS) \
-                $(DARWIN_LLVM_INCLUDES) \
-                $(FREERTOS_SIM_FLAGS_COMMON) \
-                $(FREERTOS_SIM_INCLUDES_COMMON) \
-                $(COVERAGE) \
-                $(UT_FLAGS) \
-                -fno-rtti
+CXXFLAGS := $(DEBUG) \
+$(DARWIN_LLVM_CXXFLAGS) \
+$(DARWIN_LLVM_INCLUDES) \
+$(FREERTOS_SIM_FLAGS_COMMON) \
+$(FREERTOS_SIM_INCLUDES_COMMON) \
+$(COVERAGE) \
+$(UT_FLAGS) \
+-fno-rtti
 
 LINK_BIN_FLAGS := $(LINK_BIN_FLAGS) $(COVERAGE)
 endif
 
 ifeq ("$(HOST)","Linux")
-    CFLAGS :=   $(DEBUG) \
-                $(LINUX_GNU_CFLAGS) \
-                $(LINUX_GNU_INCLUDES) \
-                $(FREERTOS_SIM_FLAGS_COMMON) \
-                $(FREERTOS_SIM_INCLUDES_COMMON) \
-                $(COVERAGE) \
-                $(UT_FLAGS)
+CFLAGS :=   $(DEBUG) \
+$(LINUX_GNU_CFLAGS) \
+$(LINUX_GNU_INCLUDES) \
+$(FREERTOS_SIM_FLAGS_COMMON) \
+$(FREERTOS_SIM_INCLUDES_COMMON) \
+$(COVERAGE) \
+$(UT_FLAGS)
 
-    CXXFLAGS := $(DEBUG) \
-                $(LINUX_GNU_CXXFLAGS) \
-                $(LINUX_GNU_INCLUDES) \
-                $(FREERTOS_SIM_FLAGS_COMMON) \
-                $(FREERTOS_SIM_INCLUDES_COMMON) \
-                $(COVERAGE) \
-                $(UT_FLAGS)
+CXXFLAGS := $(DEBUG) \
+$(LINUX_GNU_CXXFLAGS) \
+$(LINUX_GNU_INCLUDES) \
+$(FREERTOS_SIM_FLAGS_COMMON) \
+$(FREERTOS_SIM_INCLUDES_COMMON) \
+$(COVERAGE) \
+$(UT_FLAGS)
 
 LINK_BIN_FLAGS := $(LINK_BIN_FLAGS) $(COVERAGE)
 endif
--- mk/configs/compiler/gnu-debug-freertos-sim.mk
+++ mk/configs/compiler/gnu-debug-freertos-sim.mk
@@ -9,32 +9,32 @@ include $(BUILD_ROOT)/mk/configs/compiler/include_common.mk
 include $(BUILD_ROOT)/mk/configs/compiler/freertos_sim_common.mk
 
 ifeq ("$(HOST)","Darwin")
-    CFLAGS :=   $(DEBUG) \
-                $(DARWIN_LLVM_CFLAGS) \
-                $(DARWIN_LLVM_INCLUDES) \
-                $(FREERTOS_SIM_FLAGS_COMMON) \
-                $(FREERTOS_SIM_INCLUDES_COMMON)
+CFLAGS :=   $(DEBUG) \
+$(DARWIN_LLVM_CFLAGS) \
+$(DARWIN_LLVM_INCLUDES) \
+$(FREERTOS_SIM_FLAGS_COMMON) \
+$(FREERTOS_SIM_INCLUDES_COMMON)
 
-    CXXFLAGS := $(DEBUG) \
-                $(DARWIN_LLVM_CXXFLAGS) \
-                $(DARWIN_LLVM_INCLUDES) \
-                $(FREERTOS_SIM_FLAGS_COMMON) \
-                $(FREERTOS_SIM_INCLUDES_COMMON)
-                -fno-rtti
+CXXFLAGS := $(DEBUG) \
+$(DARWIN_LLVM_CXXFLAGS) \
+$(DARWIN_LLVM_INCLUDES) \
+$(FREERTOS_SIM_FLAGS_COMMON) \
+$(FREERTOS_SIM_INCLUDES_COMMON) \
+-fno-rtti
 endif
 
 ifeq ("$(HOST)","Linux")
-    CFLAGS :=   $(DEBUG) \
-                $(LINUX_GNU_CFLAGS) \
-                $(LINUX_GNU_INCLUDES) \
-                $(FREERTOS_SIM_FLAGS_COMMON) \
-                $(FREERTOS_SIM_INCLUDES_COMMON)
+CFLAGS :=   $(DEBUG) \
+$(LINUX_GNU_CFLAGS) \
+$(LINUX_GNU_INCLUDES) \
+$(FREERTOS_SIM_FLAGS_COMMON) \
+$(FREERTOS_SIM_INCLUDES_COMMON)
 
-    CXXFLAGS := $(DEBUG) \
-                $(LINUX_GNU_CXXFLAGS) \
-                $(LINUX_GNU_INCLUDES) \
-                $(FREERTOS_SIM_FLAGS_COMMON) \
-                $(FREERTOS_SIM_INCLUDES_COMMON)
+CXXFLAGS := $(DEBUG) \
+$(LINUX_GNU_CXXFLAGS) \
+$(LINUX_GNU_INCLUDES) \
+$(FREERTOS_SIM_FLAGS_COMMON) \
+$(FREERTOS_SIM_INCLUDES_COMMON)
 endif
 
 COMPILER := freertos-sim-debug-gnu
